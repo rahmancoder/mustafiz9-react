bbe4338e99d679273b713e344de9119539688c00
Mustafizur <sayhemrahman@gmail.com>
2022-01-06 18:19:37 +0600

    header navbar responsive added media responsive adding

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <>
-            <Navbar sticky="top" bg="dark" variant="dark">
+            {/* <Navbar sticky="top" bg="dark" variant="dark">
                 <Container>
                     <Navbar.Brand href="/home"><i class="fas fa-laptop-code"></i> E-Learning with Mustafiz</Navbar.Brand>
                     <Nav className="me-5 p-2 text-center">
@@ -15,9 +15,32 @@ const Header = () => {
                         <Link className="m-2 text-decoration-none" to="/userchart">User Chart</Link>
                         <Link className="m-2 text-decoration-none" to="/questions">FAQ</Link>
                         {/* <Link className="m-2" to="/notfound">Not Found</Link> */}
-                    </Nav>
+            {/* </Nav> */}
+            {/* // </Container> */}
+
+            {/* // </Navbar> */}
+
+
+            {/*  Responsive Navbar Added */}
+
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="/home"><i class="fas fa-laptop-code "></i> E-Learning with Mustafiz</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto p-2 text-center">
+                            <Button className="bg-warning"><Link className="m-2 text-decoration-none" to="/home">Home</Link> </Button>
+                            <Link className="m-3 text-decoration-none" to="/about">About</Link>
+                            <Link className="m-3 text-decoration-none" to="/services">Services</Link>
+                            <Link className="m-3 text-decoration-none" to="/userchart">User Chart</Link>
+                            <Link className="m-3 text-decoration-none" to="/questions">FAQ</Link>
+                            {/* <Link className="m-2" to="/notfound">Not Found</Link> */}
+                        </Nav>
+
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
+
 
 
         </>
--- src/Components/PopularVideos/PopularVideos.js
+++ src/Components/PopularVideos/PopularVideos.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './PopularVideos.css';
 
 const PopularVideos = () => {
     return (
         <div className="mt-4 mb-5">
 
             <h1 className="mb-4 mt-4 text-warning">Popular Course Video</h1>
-            <div className="justify-content mt-5">
+            <div className="justify-content mt-5 mustafiz-mobile ">
                 <iframe className="mx-3" width="300" height="400" src="https://www.youtube.com/embed/Es9oXr0qXrk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 <iframe className="mx-3" width="300" height="400" src="https://www.youtube.com/embed/2ObjAYV-1qs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 <iframe className="mx-3" width="300" height="400" src="https://www.youtube.com/embed/2tPnZAn8NbY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
